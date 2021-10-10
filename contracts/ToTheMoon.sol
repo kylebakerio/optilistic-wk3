b46909b1b7ac03024f10543b956e375d026cf3ba
kyle <finesttype@gmail.com>
2021-10-10 21:34:57 +0100

    minor

--- contracts/ToTheMoon.sol
+++ contracts/ToTheMoon.sol
@@ -47,9 +47,8 @@ contract ToTheMoon is SPCToken {
     }
 
     function ethToToken() private {
-        // for wk2 spec, I had auto-transfers into treasury; this has now been moved to withdraw() in SPCToken.sol 
-        // (bool sent, ) = payable(treasury).call{value: msg.value}(""); // this is moved for wk3 to be in withdraw() 
-        // require(sent, "Failed to send Ether");
+        (bool sent, ) = payable(treasury).call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
         fundraiseTotal += msg.value;
         contributions[msg.sender] += msg.value;
         ERC20._transfer(minter, msg.sender, msg.value * 5); // 5:1 initial mint value
@@ -64,11 +63,7 @@ contract ToTheMoon is SPCToken {
     }
 
     function balanceOf(address account) public view virtual override returns (uint256) {
-        if (phase < Phases.Open) {
-            return 0;
-        } else {
-            return ERC20.balanceOf(account);
-        }
+        return phase < Phases.Open ? 0 : ERC20.balanceOf(account);
     }
 
     function buy() external payable {
